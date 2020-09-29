5e3d2cd4f7f5e17f6d43e4635f930026056807f4
Paul Chin Jr <pchinjr@gmail.com>
2020-09-28 21:59:09 -0400

    api print path

--- src/http/get-api/index.js
+++ src/http/get-api/index.js
@@ -1,13 +1,17 @@
 // learn more about HTTP functions here: https://arc.codes/primitives/http
-
+let arc = require('@architect/functions')
 
 exports.handler = async function http (req) {
+
+let path = arc.static('index.js', {stagePath: false})
+console.log(path)
+
   return {
     statusCode: 200,
     headers: {
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
       'content-type': 'application/json'
     },
-    body: JSON.stringify({"message":"Praise Cage!"})
+    body: JSON.stringify(`{"jsPath":"${path}"}`)
   }
 }
